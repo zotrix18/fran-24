b237d68afa572f635e9bed8c081503179d71221f
zotrix18 <facuelkpo49@gmail.com>
2023-09-22 18:31:13 -0300

    añadido nav extras

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,12 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/producto') }}">{{ __('Productos') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/categoria') }}">{{ __('Categorias') }}</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
